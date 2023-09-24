414fd8d3a0cce7706e5f5a4153b7c9e0918dede9
unknown <sumiartinisrirahayu@gmail.com>
2023-09-24 17:10:52 +0700

    [Soal 2] Membuat program untuk menjumlahkan kuantitas untuk setiap storageId & menambahkan komentar di soal 1

--- soal1.js
+++ soal1.js
@@ -1,23 +1,32 @@
+// Mengimpor modul readline untuk berinteraksi dengan pengguna melalui terminal
 const readline = require('readline');
 
+// Membuat antarmuka readline
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+    input: process.stdin,  // Masukan dari terminal
+    output: process.stdout  // Keluaran ke terminal
 });
 
+// Menanyakan pengguna untuk memasukkan bilangan genap
 rl.question("Masukkan bilangan genap: ", (input) => {
+    // Mengonversi input pengguna ke tipe data bilangan bulat
     const x = parseInt(input);
 
+    // Memeriksa apakah input adalah bilangan bulat
     if (isNaN(x)) {
         console.log("Input yang Anda masukkan bukan bilangan bulat");
     } else if (x < 0) {
+        // Memeriksa apakah input negatif
         console.log("Tidak bisa input bilangan negatif");
     } else if (x % 2 !== 0) {
+        // Memeriksa apakah input ganjil
         console.log("Tidak bisa input bilangan ganjil");
     } else {
+        // Menghitung akar pangkat 2 dari input
         const result = Math.sqrt(x);
         console.log(`Akar pangkat 2 dari ${x} adalah ${result}`);
     }
 
+    // Menutup antarmuka readline setelah selesai
     rl.close();
 });
